d18cac4fe382549344cee6fb9e7ade425eb328fe
GearFifth <ilija.beslin@gmail.com>
2025-05-22 10:37:50 +0200

    feat: added recommend users endpoint

---
+++ Backend/instagram/web/src/main/java/gearfifth/com/example/controllers/RecommendationController.java
@@ -0,0 +1,30 @@
+package gearfifth.com.example.controllers;
+
+import gearfifth.com.example.dtos.users.responses.UserProfileResponse;
+import gearfifth.com.example.recommendations.IRecommendationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@RestController
+@Validated
+@RequestMapping("api/v1/recommendations")
+public class RecommendationController {
+    private final IRecommendationService service;
+
+    @GetMapping("/users")
+    public ResponseEntity<Collection<UserProfileResponse>> recommendUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return ResponseEntity.ok(service.recommendUsers(page, size));
+    }
+
+}
