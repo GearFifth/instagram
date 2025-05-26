a41a9bc6b50541070fe4b55b8f46677ee054ae72
GearFifth <ilija.beslin@gmail.com>
2025-05-26 09:45:38 +0200

    feat: added not found page

---
+++ Frontend/instagram/src/app/shared/components/not-found/not-found.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {ROUTE_PATHS} from "../../../core/constants/routes";
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css'
+})
+export class NotFoundComponent {
+  constructor(private router: Router) {
+  }
+
+  goToHome(){
+    this.router.navigate([ROUTE_PATHS.POSTS_ROOT]);
+  }
+}
